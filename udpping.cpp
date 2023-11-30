5fffea694c34211eb053dd5d42d5ea6d0c57c1b7
aa35037123 <wesleyshi0321@gmail.com>
2023-11-30 16:26:42 +0000

    go-back-n server first try

---
+++ udpping.cpp
@@ -0,0 +1,178 @@
+/*
+ * Lab problem set for INP course
+ * by Chun-Ying Huang <chuang@cs.nctu.edu.tw>
+ * License: GPLv2
+ */
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define MAXLINE 300 //60000
+#define WINDOW_SIZE 200
+#define QUEUE_CAPACITY 10 //1000
+#define errquit(m) { perror(m); exit(-1); }
+using namespace std;
+
+char buffer[MAXLINE];
+
+class Packet{
+public:
+	int seq;
+	int ack;
+	bool fin;
+	uint16_t cksum;
+	int len; // only length of data
+	string filename;
+	bool fileEnd;
+	unsigned char* data;
+
+	bool isAcked;
+
+	Packet(int _seq, bool _fin, int _len, char* _filename, bool _fileEnd, char* _data){
+		seq = _seq; ack = 0; fin = _fin; len = _len;
+		filename = _filename; fileEnd = _fileEnd;
+		filename = _filename;
+		data = new unsigned char[_len];
+		memcpy(data, _data, _len);
+		isAcked = false;
+	};
+	// Destructor to free the allocated memory
+    // ~Packet() {
+    //     delete[] data;
+    // }
+	void calculateCksum(){
+		unsigned short *ptr = (unsigned short*)data;
+		cksum = ptr[0];
+		int round = len / 2;
+		for(int i = 1; i < round; i++){
+			cksum = cksum ^ ptr[i];
+		}
+	}
+	void print(){
+		printf("====================================================\n");
+		printf("seq: %d\nACK: %d\nfin: %d\ncksum: %hu\nfilename: %s\nContent Length: %d\nfileEnd: %d\n%s\n",
+			seq, ack, fin, cksum, filename.c_str(), len, fileEnd, data);
+		printf("====================================================\n");
+	}
+	void send(int sockfd){
+		bzero(&buffer, sizeof(buffer));
+		sprintf(buffer, "%d\n%d\n%d\n%hu\n%s\n%d\n%s",
+			seq, ack, fin, cksum, filename.c_str(), fileEnd, data);
+		int n;
+		if((n = write(sockfd, buffer, sizeof(buffer))) < 0) errquit("write");
+	}
+};
+
+static int s = -1;
+static struct sockaddr_in sin;
+socklen_t servlen = sizeof(sin);
+queue<Packet> sendQueue;
+queue<Packet> waitQueue;
+int cnt = WINDOW_SIZE;
+int totalFile;
+char* fileDir;
+
+void sending();
+void receiving();
+void prepare();
+
+int main(int argc, char *argv[]) {
+	if(argc < 5) {
+		return -fprintf(stderr, "usage: %s ...<path-to-read-files> <total-number-of-files> <port> <server-ip-address>\n", argv[0]);
+	}
+
+	fileDir = argv[1];
+	totalFile = atoi(argv[2]);
+
+	srand(time(0) ^ getpid());
+
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(strtol(argv[argc-2], NULL, 0));
+	if(inet_pton(AF_INET, argv[argc-1], &sin.sin_addr) != 1) {
+		return -fprintf(stderr, "** cannot convert IPv4 address for %s\n", argv[1]);
+	}
+
+	if((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+		errquit("socket");
+
+	prepare();
+	
+
+	close(s);
+}
+
+void prepare(){
+	int seq = 1;
+	int curFile = 0;
+	int n;
+	char buf[MAXLINE];
+	for(curFile = 0; curFile < totalFile; curFile++){
+		char filename[256];
+		sprintf(filename, "%s/%06d", fileDir, curFile);
+		fstream file;
+		file.open(filename);
+		if(file.fail()) errquit("fstream open file");
+		
+		bzero(&buf, sizeof(buf));
+		while(file.read(buf, sizeof(buf))){
+			while(sendQueue.size() >= QUEUE_CAPACITY) sleep(1);
+			// Packet(int _seq, bool _fin, int _len, string _filename, bool _fileEnd, char* _data)
+			Packet tmp(seq, 0, sizeof(buf), filename, 0, buf);
+			tmp.calculateCksum();
+			tmp.print();
+			// TODO: lock
+			sendQueue.push(tmp);
+			seq++;
+			bzero(&buf, sizeof(buf));
+		}
+		// while(file.read(buf, sizeof(buf))){/*read umtil EOF*/
+		// 	while(sendQueue.size() >= QUEUE_CAPACITY) sleep(1);
+		// 	// Packet(int _seq, bool _fin, int _len, string _filename, bool _fileEnd, char* _data)
+		// 	Packet tmp(seq, 0, sizeof(buf), filename, 0, buf);
+		// 	tmp.calculateCksum();
+		// 	tmp.print();
+		// 	// TODO: lock
+		// 	sendQueue.push(tmp);
+		// 	seq++;
+		// 	bzero(&buf, sizeof(buf));
+		// }
+		cout << "file gcount: " << file.gcount() << '\n';
+		Packet tmp(seq, 0, file.gcount(), filename, 1, buf);
+		tmp.calculateCksum();
+		tmp.print();
+		// TODO: lock
+		sendQueue.push(tmp);
+		seq++;
+		bzero(&buf, sizeof(buf));
+		file.close();
+		while(!sendQueue.empty()){
+			Packet temp = sendQueue.front();
+			sendQueue.pop();
+			temp.send(s);
+		}
+		for(int i = 1; i < seq; i++){
+			bzero(&buf, sizeof(buf));
+			if((n = recvfrom(sockfd, buf, MAXLINE, MSG_WAITALL, (struct sockaddr *) &sin, &servlen)) < 0){
+				perror("recvfrom");
+				break;
+			}
+		}
+		
+	}
+}
